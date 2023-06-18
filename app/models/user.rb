22e12c0220fe01738965e9acfc75c397f4d66c3b
mryzkt1999 <oram1501@yahoo.co.jp>
2023-06-18 19:58:54 +0900

    Devise導入、ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: " is invalid. Input full-width characters
+    " }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: " is invalid. Input full-width characters
+    " }
+  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: " is invalid. Input full-width characters
+    " }
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: " is invalid. Input full-width characters
+    " }
+  validates :birthday, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
