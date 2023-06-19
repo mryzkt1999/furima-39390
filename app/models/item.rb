cf68d406dd0139268c70854ae41942f2092e3233
mryzkt1999 <oram1501@yahoo.co.jp>
2023-06-19 20:47:04 +0900

    ActiveStorage導入、バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :item_category, item_prefecture, item_sales_status, item_scheduled_delivery, item_shipping_fee_status
+  belongs_to :item_category, :item_prefecture, :item_sales_status, :item_scheduled_delivery, :item_shipping_fee_status
+  has_one_attached :image
  
-  validates :item_name, :item_info, item_price, presence: true
-  validates :item_category_id, item_sales_status_id, item_shipping_fee_status_id, item_prefecture_id, item_scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :image, :item_name, :item_info, :item_price, presence: true
+  validates :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank"} 
 
 end
