ee1d23b105a4b5d60edd3381f17944591a1c54f2
mryzkt1999 <oram1501@yahoo.co.jp>
2023-06-30 11:59:25 +0900

    挙動確認後の修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -11,8 +11,8 @@ class OrderAddress
     validates :house_number
     validates :phone_number, numericality: { only_integer: true, message: 'is invalid' },
                              length: { minimum: 10, message: 'is too short' }
-    validates :phone_number, length: { maximum: 11, message: 'is invalid' }
-  end
+   end
+  validates :phone_number, length: { maximum: 11, message: 'is invalid' } 
   validates :item_prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
